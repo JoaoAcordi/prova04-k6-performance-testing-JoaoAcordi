39f209567e3c3f6f7064027061f50af403b2e145
João Pedro Acordi <88681190+JoaoAcordi@users.noreply.github.com>
2025-11-17 19:32:18 -0300

    completo

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,23 +1,27 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+
 import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const getContactsDuration = new Trend('get_contacts_duration');
+export const RateContentOK = new Rate('content_ok');
 
 export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
-  },
   stages: [
-    { duration: '10s', target: 2 },
-    { duration: '10s', target: 4 },
-    { duration: '10s', target: 6 }
-  ]
+    { duration: '30s', target: 7 },
+    { duration: '2m30s', target: 92 },
+    { duration: '30s', target: 0 }
+  ],
+
+  thresholds: {
+    http_req_duration: ['p(90)<6800'],
+
+    content_ok: ['rate>0.75'],
+
+    get_contacts_duration: ['p(90)<6800']
+  }
 };
 
 export function handleSummary(data) {
@@ -28,7 +32,7 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://www1.satc.edu.br/portais/acesso/public/#/';
 
   const params = {
     headers: {
@@ -36,15 +40,13 @@ export default function () {
     }
   };
 
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
+  const res = http.get(baseUrl, params);
 
   getContactsDuration.add(res.timings.duration);
 
-  RateContentOK.add(res.status === OK);
+  RateContentOK.add(res.status < 400);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'Status 200 ou 3xx': r => r.status < 400
   });
 }
