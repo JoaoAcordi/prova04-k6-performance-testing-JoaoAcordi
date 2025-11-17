e1ebe785f030923e5dd3f0ac57434051beb6ca89
João Pedro Acordi <88681190+JoaoAcordi@users.noreply.github.com>
2025-11-17 19:42:33 -0300

    completo

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,52 +1,51 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-
 import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
-export const getContactsDuration = new Trend('get_contacts_duration');
-export const RateContentOK = new Rate('content_ok');
+// Métricas personalizadas
+export const activityTrend = new Trend('getActivityDuration', true);
+export const okRate = new Rate('RateRequestOK');
 
+// Configurações do teste
 export const options = {
-  stages: [
-    { duration: '30s', target: 7 },
-    { duration: '2m30s', target: 92 },
-    { duration: '30s', target: 0 }
-  ],
-
   thresholds: {
-    http_req_duration: ['p(90)<6800'],
+    'http_req_failed': ['rate<0.25'],
+    'getActivityDuration': ['p(90)<6800'],
+    'RateRequestOK': ['rate>0.75'],
+  },
 
-    content_ok: ['rate>0.75'],
-
-    get_contacts_duration: ['p(90)<6800']
-  }
+  stages: [
+    { duration: '10s', target: 7 },   // warm-up
+    { duration: '60s', target: 92 },  // ramp-up
+    { duration: '110s', target: 92 }, // sustentação
+    { duration: '30s', target: 7 }    // ramp-down
+  ]
 };
 
+// Relatórios
 export function handleSummary(data) {
   return {
-    './src/output/index.html': htmlReport(data),
+    'src/output/index.html': htmlReport(data),
     stdout: textSummary(data, { indent: ' ', enableColors: true })
   };
 }
 
+// Execução de cada VU
 export default function () {
-  const baseUrl = 'https://www1.satc.edu.br/portais/acesso/public/#/';
+  const API = 'https://fakerestapi.azurewebsites.net/api/v1/Activities';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  const response = http.get(API, {
+    headers: { 'Content-Type': 'application/json' }
+  });
 
-  const res = http.get(baseUrl, params);
+  // Alimenta as métricas
+  activityTrend.add(response.timings.duration);
+  okRate.add(response.status === 200);
 
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status < 400);
-
-  check(res, {
-    'Status 200 ou 3xx': r => r.status < 400
+  // Validação
+  check(response, {
+    'Status é 200': () => response.status === 200
   });
 }
